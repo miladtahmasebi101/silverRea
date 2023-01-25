82fce5ff8599d49003d7a82b586d5b11871294bf
Milad <tahmasebimilad101@gmail.com>
2023-01-25 23:43:51 +0330

    header1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './Page/landing/landing.component';
+import { SearchComponent } from './Page/search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LandingComponent
+  },
+  {
+    path: 'search',
+    component: SearchComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Page/header/header.component';
+import { LandingComponent } from './Page/landing/landing.component';
+import { FooterComponent } from './Page/footer/footer.component';
+import { SearchComponent } from './Page/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LandingComponent,
+    FooterComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
